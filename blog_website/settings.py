a4ad2288645de87b023bbf264ff859dd55e4c2c2
EAGLEEARTH <ufukcicek199@gmail.com>
2023-08-28 22:41:30 +0300

    changes

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -141,13 +141,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
 
-    os.path.join(BASE_DIR, 'assets')
+    os.path.join(BASE_DIR, 'static_files')
 ]
-MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
