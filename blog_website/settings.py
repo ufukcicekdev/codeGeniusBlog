aeadb318c729bfe34f7bf864bd66001e89843739
EAGLEEARTH <ufukcicek199@gmail.com>
2023-09-01 13:27:46 +0300

    pillow added

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -176,6 +176,7 @@ SESSION_TIMEOUT_REDIRECT = 'home'
 CKEDITOR_UPLOAD_PATH = "uploads/"
 #CKEDITOR_STORAGE_BACKEND = 'django.core.files.storage.FileSystemStorage'
 FILEBROWSER_REMOVE_DIALOG_TABS = 'image:Upload'
+CKEDITOR_IMAGE_BACKEND = "pillow"
 
 CKEDITOR_CONFIGS = {
    'default': {
