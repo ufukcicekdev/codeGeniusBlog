dd88201bfccc064e254bd704e9ea4b3586d166c3
EAGLEEARTH <ufukcicek199@gmail.com>
2023-09-02 09:31:39 +0300

    push

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["codegenius.blog","*"]
+ALLOWED_HOSTS = ["codegenius.blog"]
 
 CSRF_TRUSTED_ORIGINS = ["https://*.codegenius.log"]
 # Application definition
