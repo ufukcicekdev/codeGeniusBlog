9c0a91dbba67ee8bf6973dfce7931638575d50ef
EAGLEEARTH <ufukcicek199@gmail.com>
2023-09-02 22:30:15 +0300

    push

--- blog_website/urls.py
+++ blog_website/urls.py
@@ -28,12 +28,7 @@ urlpatterns = [
     path('add_blog/uploadf/',   editorjs_file_upload, name='editorjs_file_upload'),
     path('profile/uploadi/',    editorjs_image_upload, name='editorjs_image_upload'),
     path('profile/uploadf/',    editorjs_file_upload, name='editorjs_file_upload'),
-]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
