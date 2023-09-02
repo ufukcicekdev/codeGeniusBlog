8b39b63235dcc2ea388e43ac0abb1181f34d160a
EAGLEEARTH <ufukcicek199@gmail.com>
2023-09-02 22:54:02 +0300

    push

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -164,7 +164,7 @@ USE_TZ = True
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
