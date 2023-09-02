8f524240e3144e0248d91f411d67ca07f8fc2feb
EAGLEEARTH <ufukcicek199@gmail.com>
2023-09-02 12:58:20 +0300

    push

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -162,10 +162,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-MEDIA_URL = 'https://ufukcicekdev.fra1.digitaloceanspaces.com/'
 
-
-MEDIA_ROOT = MEDIA_URL  + 'media/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
